2114f1d1fd6c6f368132d3c8c1a102c6638fc6e4
ploy <ploychompoo@geniustree.co.th>
2023-06-26 16:09:10 +0700

    add method treeWalk

--- src/main/java/org/example/binaryTree/App.java
+++ src/main/java/org/example/binaryTree/App.java
@@ -5,25 +5,34 @@ public class App
     public static void main( String[] args )
     {
         Node node = new Node();
-         //Inserting nodes
-        node.root = new Node(50);
+        node.root = new Node(5);
+        node.insert(node.root,3);
+        node.insert(node.root,4);
+        node.insert(node.root,1);
+        node.insert(node.root,2);
+
+        node.insert(node.root,5);
+        node.insert(node.root,8);
+        node.insert(node.root,9);
         node.insert(node.root,10);
-        node.insert(node.root,20);
-        node.insert(node.root,70);
-        node.insert(node.root,60);
-
-        System.out.println("root : " + node.root.toString());
-
-        int key = 6;
-        int key2 = 10;
-        node.search(node.root, key);
-        node.search(node.root, key2);
-
-
+        node.insert(node.root,6);
+        node.insert(node.root,7);
+        //search
+        node.search(node.root, 6);
+        node.search(node.root, 60);
+        System.out.println();
+        //print tree
+        node.treeWalk(node.root);
+        System.out.println();
+        //show all node
         node.print(node.root);
-        int keyToDelete = 60;
-        node.delete(node.root,keyToDelete);
+        //delete node
+        node.delete(node.root,8);
         System.out.println();
         node.print(node.root);
+
+
+        System.out.println();
+        node.treeWalk(node.root);
     }
 }
--- src/main/java/org/example/binaryTree/Node.java
+++ src/main/java/org/example/binaryTree/Node.java
@@ -1,49 +1,53 @@
 package org.example.binaryTree;
 
 public class Node {
-     Node root;
-     private Node left , right;
+    Node root;
+    private Node left, right;
     private int value;
+    private int iCount = 1;
 
     public Node() {
         root = null;
     }
-    Node(int value){
+
+    Node(int value) {
         this.value = value;
     }
-    public Node insert(Node node,int key){
+
+    public Node insert(Node node, int key) {
         if (node == null) {
             node = new Node(key);
             return node;
         }
-        if(key < node.value){
-            node.left = insert(node.left , key);
-            System.out.println("node L : " + node.left);
+        if (key < node.value) {
+            node.left = insert(node.left, key);
+            //System.out.println("node L : " + node.left);
 
         } else if (key > node.value) {
-            node.right = insert(node.right , key);
-            System.out.println("node R: " +node.right);
+            node.right = insert(node.right, key);
+            //System.out.println("node R: " +node.right);
         }
         return node;
     }
 
     public void search(Node node, int key) {
-        if(node == null){
+        if (node == null) {
             System.out.println(key + " not found");
         } else {
-            if(node.value == key){
+            if (node.value == key) {
                 System.out.println(key + " found");
             } else {
-                if (node.value < key){
-                    search(node.right,key);
+                if (node.value < key) {
+                    search(node.right, key);
                 } else {
-                    search(node.left , key);
+                    search(node.left, key);
                 }
             }
         }
     }
+
     Node delete(Node node, int key) {
-        if (node == null){
+        if (node == null) {
             return node;
         }
 
@@ -72,7 +76,7 @@ public class Node {
         return min;
     }
 
-    public void print(Node node){
+    public void print(Node node) {
         if (node != null) {
             print(node.left);
             System.out.print(node.value + " ");
@@ -80,8 +84,37 @@ public class Node {
         }
     }
 
-    @Override
-    public String toString() {
-        return ""+value;
+    public void printTree(int count) {
+        for(int i = 0; i < count; i++){
+            System.out.print("-");
+        }
+    }
+    public void treeWalk(Node node){
+        if(node != null) {
+            if(node.value == root.value){
+                System.out.println("Root " + node.value);
+            }
+            if (node.left != null) {
+                iCount++;
+                if(iCount == 2 && node.left != root.left){
+                    iCount += 1;
+                }
+                System.out.print("  ");
+                printTree(iCount);
+                System.out.println(" " +node.left.value);
+                treeWalk(node.left);
+            }
+            if (node.right != null) {
+                iCount++;
+                if(iCount == 2 && node.right != root.right){
+                    iCount += 1;
+                }
+                System.out.print("  ");
+                printTree(iCount);
+                System.out.println(" " + node.right.value);
+                treeWalk(node.right);
+            }
+        }
+        iCount = 1;
     }
 }
